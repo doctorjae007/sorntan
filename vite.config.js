c6f47b666cb5daeed507550d475474d3a7a19fa8
jaejae <jaeautobot@gmail.com>
2026-07-11 12:22:36 +0700

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/sorntan/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(),tailwindcss()],
 })
